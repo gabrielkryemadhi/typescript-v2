bc499d6d4bcf44c166a29bcade57b84c3d149f78
Gabriel <Kryemadhigabriel@gmail.com>
2023-04-02 15:02:42 +0200

    Ex. 13, 14, 15 done

--- exercise-1-complete-the-class-start.ts
+++ exercise-1-complete-the-class-start.ts
@@ -7,19 +7,20 @@ class Currency {
     // Hint: Look at the type of values being passed when `new Currency()`
     // is called below.
 
-    name;
-    code;
-    symbol;
+    private name: string;
+    private code: string;
+    private symbol: string;
 
     // Add types to the constructor parameters.
 
-    constructor(name, code, symbol) {
+    constructor(name: string, code: string, symbol: string) {
         this.name = name;
 
         // Store the `code` and `symbol` parameter values in
         // their corresponding class fields.
-
-
+        
+        this.code = code;
+        this.symbol = symbol;
     }
 
     // Methods are public by default.
@@ -28,13 +29,13 @@ class Currency {
     // Add a return type for this method
     // Hint: Look at the inferred return type.
 
-    describe() {
+    public describe(): void {
         // Replace the `null` values below with the appropriate field values.
         // Hint: Access field values with: this.fieldName
 
-        let description = `The ${null} currency `;
-        description += `has the code ${null} `;
-        description += `and uses the symbol ${null}.`;
+        let description = `The ${this.name} currency `;
+        description += `has the code ${this.code} `;
+        description += `and uses the symbol ${this.symbol}.`;
 
         console.log(description);
     }
@@ -48,7 +49,7 @@ console.log(currencyNaira);
 
 // Call the `describe()` method on the `currencyNaira` object.
 
-
+console.log(currencyNaira.describe());
 
 // ----
 
@@ -58,7 +59,7 @@ console.log(currencyUsDollar);
 
 // Call the `describe()` method on the `currencyUsDollar` object.
 
-
+console.log(currencyUsDollar.describe());
 
 // ----
 
